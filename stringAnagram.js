b86410315c4ced5b135aa3de3607409c60ce6006
onlyhamzashahid <hamza.7285618@gmail.com>
2024-05-21 10:50:24 +0500

    String Anagram

---
+++ stringAnagram.js
@@ -0,0 +1,30 @@
+// String anagram
+// 'hello' -> 'llheo'
+// 'hamza' -> 'aahmz'
+
+function isAnagram(string1, string2) {
+    if(string1.length !== string2.length){
+        return false;
+    }
+
+    let counter = {}
+    for (let letter of string1) {
+        counter[letter] = (counter[letter]  || 0) + 1
+        console.log(counter[letter])
+        
+    }
+
+    for(let items of string2){
+        if (!counter[items]) {
+            return false
+        }
+        counter[items]-=1
+    }
+    return true
+}
+
+
+const check = isAnagram('hello', 'llheo')
+console.log(check)
+
+// linear time complexity o(n)
